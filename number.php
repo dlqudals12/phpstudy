2c05f5c5bcf629793045c2040ebbe57d1dffd3fa
github dlqudals12 <github dlqudalswkd@gmial.com>
2022-06-15 21:20:29 +0900

    2022-06-15-21:20

---
+++ number.php
@@ -0,0 +1,17 @@
+<!Doctype html>
+<html>
+
+<body>
+    <h1>1+1</h1>
+    <?php
+    echo 1;
+    echo 1+1;
+    ?>
+    <h1>2-1</h1>
+    <?php
+    echo 2-1;
+    echo 2*2;
+    echo 4/2;
+    ?>
+</body>
+</html>
---
+++ string.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<body>
+<h1>String</h1>
+<?php
+echo "Hello \"W\"orld";
+?>
+</body>
+</html>
